501a003c01bba8ac3b95f372527ac7f0853e7b90
Muffinka <Kitty_wild_1@yahoo.com>
2022-09-09 03:46:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./favicon.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      Hi
+    </div>
+  );
+}
+
+export default App;
