fafba342b2ed6ec47a2ef72b9d13144139ec794a
Muffinka <Kitty_wild_1@yahoo.com>
2022-09-14 03:43:18 +0300

    Search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./favicon.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,7 +8,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      Hi
+      <Dictionary />
+      <div className="text-center">
+        Go to my&nbsp;&nbsp;
+        <a
+          href="https://github.com/Muffinka/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+          alt="Github repository @Muffinka"
+        >
+          GitHub repository
+        </a>
+        .
+      </div>
     </div>
   );
 }
