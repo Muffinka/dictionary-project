741221cd33643b13c1042ea27c412e45d86b7392
Muffinka <Kitty_wild_1@yahoo.com>
2022-09-17 03:49:45 +0300

    Troubleshooting

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         <span className="material-symbols-outlined">volume_up</span>
       </a>
       {props.phonetic.text}
